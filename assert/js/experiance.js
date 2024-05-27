bcfafdcf288ff7d3f17aa1710217482a28a9dc50
Dasun Theekshana <dasun.theekshana.git@gmail.com>
2024-05-27 16:29:22 +0530

    responsive experiance section

--- assert/js/experiance.js
+++ assert/js/experiance.js
@@ -7,13 +7,19 @@ const options = {
 };
 
 function callback(entries, observer) { 
+    const isSmallScreen = window.matchMedia('(max-width: 767px)').matches;
+
     entries.forEach(entry => {
         if(entry.isIntersecting) {
             const columnIndex = Array.from(entry.target.parentNode.children).indexOf(entry.target) % 3;
             if(columnIndex === 0) {
                 entry.target.classList.add('animate-right');
             } else if(columnIndex === 2) {
-                entry.target.classList.add('animate-left');
+                if (isSmallScreen) {
+                    entry.target.classList.add('animate-right');
+                }else{
+                    entry.target.classList.add('animate-left');
+                }
             }
         } else {
             entry.target.classList.remove('animate-right', 'animate-left');
